58ccb62cced713fa23523b6b4e27bcb33a02d9de
Artur <Artur@DESKTOP-M84G0L8>
2022-07-03 00:29:24 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace Calculadoras
+{
+    public static class VariaveisGlobais
+    {
+        //VARS GLOBAL
+        public const double Pi = 3.14;
+    }
+    class UserInterface
+    {
+        public static void Main(string[] args)
+        {
+            Splash();
+            Selecao();
+        }
+        static void Splash()
+        {
+            Console.WindowWidth = 160;
+            Console.WindowHeight = 25;
+            Console.WriteLine
+(@"
+===========================================================================================================================================================
+ █████╗ ██████╗ ████████╗██╗   ██╗██████╗  ██████╗███████╗ ██████╗  █████╗     ███████╗ ██████╗ ███████╗████████╗██╗    ██╗ █████╗ ██████╗ ███████╗███████╗
+██╔══██╗██╔══██╗╚══██╔══╝██║   ██║██╔══██╗██╔════╝╚════██║██╔════╝ ██╔══██╗    ██╔════╝██╔═══██╗██╔════╝╚══██╔══╝██║    ██║██╔══██╗██╔══██╗██╔════╝██╔════╝
+███████║██████╔╝   ██║   ██║   ██║██████╔╝███████╗    ██╔╝███████╗ ╚█████╔╝    ███████╗██║   ██║█████╗     ██║   ██║ █╗ ██║███████║██████╔╝█████╗  ███████╗
+██╔══██║██╔══██╗   ██║   ██║   ██║██╔══██╗██╔═══██╗  ██╔╝ ██╔═══██╗██╔══██╗    ╚════██║██║   ██║██╔══╝     ██║   ██║███╗██║██╔══██║██╔══██╗██╔══╝  ╚════██║
+██║  ██║██║  ██║   ██║   ╚██████╔╝██║  ██║╚██████╔╝  ██║  ╚██████╔╝╚█████╔╝    ███████║╚██████╔╝██║        ██║   ╚███╔███╔╝██║  ██║██║  ██║███████╗███████║
+╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝ ╚═════╝   ╚═╝   ╚═════╝  ╚════╝     ╚══════╝ ╚═════╝ ╚═╝        ╚═╝    ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
+===========================================================================================================================================================");
+            Console.Beep(800, 500);
+            Thread.Sleep(300);
+            Console.Clear();
+            Console.WindowWidth = 100;
+        }
+        static void Selecao()
+        {
+            //VARS LOCAL
+            bool verificar;
+            int userout;
+            //
+            Console.WriteLine("==========================================\n" +
+                                   "Qual calculadora deseja utiizar?\n" +
+                                           "[1] Pitagoras\n" +
+                                      "[2] Volume da esfera\n" +
+                                          "[3] Area do circulo\n" +
+                                      "[4] Comprimento do circulo\n" +
+                              "==========================================");
+            while (true)
+            {
+                Console.Write("Sua escolha: ");
+                verificar = int.TryParse(Console.ReadLine(), out (userout));
+                if (verificar) break;
+                Console.Clear();
+                Console.WriteLine("Erro, opção invalida.");
+            }
+            switch (userout)
+            {
+                case 1:
+                    CalcPitagoras.Principal();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+        public static void Fim()
+        {
+            Console.WriteLine("Obrigado por utilizar o meu Software, Artur6768, 2022");
+            Console.WriteLine("Retornado ao Terminal...");
+            Environment.ExitCode = -1;
+        }
+    }
+    class CalcPitagoras
+    { 
+        public static void Principal()
+        {
+            //VARS local
+            bool verificar;
+            double cat1;
+            double cat2;
+            double catqua1;
+            double catqua2;
+            double resultado;
+            //
+            while (true)
+            {
+                Console.WriteLine("Por favor, insira o valor do primeiro cateto.");
+                verificar = double.TryParse(Console.ReadLine(), out (cat1));
+                if (verificar) break;
+                Console.Clear();
+                Console.WriteLine("Erro, valor invalido.");
+            }
+            while (true)
+            {
+                Console.WriteLine("Por favor, insira o valor do segundo cateto.");
+                verificar = double.TryParse(Console.ReadLine(), out (cat2));
+                if (verificar) break;
+                Console.Clear();
+                Console.WriteLine("Erro, valor invalido.");
+            }
+            catqua1 = (cat1 * cat1);
+            catqua2 = (cat2 * cat2);
+            resultado = Math.Sqrt(catqua1 + catqua2);
+            Console.WriteLine($"O resultado será: {resultado}");
+            Console.WriteLine("Pressione qualquer tecla para continuar (No futuro, será possivel escolher outras opções");
+            _ = Console.ReadLine();
+            UserInterface.Fim();
+        }
+    }
+}
---
+++ ConsoleApp4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Diveras calculadoras")]
+[assembly: AssemblyDescription("Aplicativo de diversas calculadoras, criado para estudar um pouco de C#")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Artur6768 Softwares")]
+[assembly: AssemblyProduct("ConsoleApp4")]
+[assembly: AssemblyCopyright("Artur6768 Copyright ©  2022")]
+[assembly: AssemblyTrademark("Artur6768 Softwares")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("47badf6a-47ec-45f6-88f0-695561318320")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1")]
+[assembly: AssemblyFileVersion("2.1")]
